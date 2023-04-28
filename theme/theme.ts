20853cf27d706dd0a75e11e5d3724372997ba925
yutaYAMAMORI <yamamori929yuta@gmail.com>
2023-04-28 18:01:19 +0900

    add: created Theme and set App

---
+++ theme/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material'
+
+export const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#212196',
+			light: '#2A2ABB',
+			dark: '#191970',
+		},
+	},
+})
