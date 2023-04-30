3980899c16fd7bb2c25963d93a7a651673c20928
yutaYAMAMORI <yamamori929yuta@gmail.com>
2023-04-30 16:45:21 +0900

    add: create ArticleCard component

---
+++ components/molecules/ArticleCard/index.tsx
@@ -0,0 +1,50 @@
+import {
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Typography,
+} from '@mui/material'
+import Image from 'next/image'
+import { NextPage } from 'next'
+import { FcLikePlaceholder } from 'react-icons/all'
+
+type ArticleCardProps = {
+	imageSrc?: string
+	imageAlt?: string
+	title: string
+	describeMessage: string
+}
+
+const ArticleCard: NextPage<ArticleCardProps> = (props) => {
+	const { imageSrc, imageAlt, title, describeMessage } = props
+
+	return (
+		<Card sx={{ maxWidth: 345 }}>
+			{imageSrc && imageAlt && (
+				<CardMedia sx={{ height: 160 }}>
+					<Image
+						src={imageSrc}
+						alt={imageAlt}
+						width={345}
+						height={160}
+						style={{ objectFit: 'cover' }}
+					/>
+				</CardMedia>
+			)}
+			<CardContent>
+				<Typography variant='h5' component='div'>
+					{title}
+				</Typography>
+				<Typography variant='body2' color='text.secondary'>
+					{describeMessage}
+				</Typography>
+			</CardContent>
+			<CardActions disableSpacing>
+				<FcLikePlaceholder size={24} />
+			</CardActions>
+		</Card>
+	)
+}
+
+export default ArticleCard
---
+++ stories/molecules/ArticleCard.stories.tsx
@@ -0,0 +1,31 @@
+import ArticleCard from '../../components/molecules/ArticleCard'
+import { Meta, StoryObj } from '@storybook/react'
+import { ThemeProvider } from '@mui/material'
+import { theme } from '../../theme/theme'
+
+const meta: Meta<typeof ArticleCard> = {
+	title: 'molecules/ArticleCard',
+	component: ArticleCard,
+	decorators: [
+		(Story) => {
+			return (
+				<ThemeProvider theme={theme}>
+					<Story />
+				</ThemeProvider>
+			)
+		},
+	],
+}
+
+export default meta
+
+type Story = StoryObj<typeof ArticleCard>
+
+export const Primary: Story = {
+	args: {
+		imageSrc: '/Profile/yamamori-rice.jpg',
+		imageAlt: 'Mountain Forest',
+		title: 'Mountain Forest',
+		describeMessage: 'Sample describe message',
+	},
+}
