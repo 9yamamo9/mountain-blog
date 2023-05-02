46dac8e0a2c83c4b66d5a3f91d218a84b675351e
yutaYAMAMORI <yamamori929yuta@gmail.com>
2023-05-02 22:29:34 +0900

    mod: display Footer component in the top page

--- components/organisms/Footer/index.tsx
+++ components/organisms/Footer/index.tsx
@@ -1,20 +1,29 @@
-import { Box, Typography } from '@mui/material'
+import { BottomNavigation, Box, Paper, Typography } from '@mui/material'
 import FooterItems from '../../molecules/FooterItems'
 
 const Footer = () => {
 	return (
-		<Box
-			display='flex'
-			flexDirection='column'
-			justifyContent='center'
-			alignItems='center'
-			bgcolor='lightgrey'
-			p={1}>
-			<FooterItems />
-			<Typography variant='body2' mt={1}>
-				{"© 2023 Mountain Forest's blog"}
-			</Typography>
-		</Box>
+		<Paper
+			sx={{
+				position: 'fixed',
+				bottom: 0,
+				left: 0,
+				right: 0,
+				zIndex: (theme) => theme.zIndex.drawer + 1,
+			}}>
+			<Box
+				display='flex'
+				flexDirection='column'
+				justifyContent='center'
+				alignItems='center'
+				bgcolor='lightgrey'
+				p={1}>
+				<FooterItems />
+				<Typography variant='body2' mt={1}>
+					{"© 2023 Mountain Forest's blog"}
+				</Typography>
+			</Box>
+		</Paper>
 	)
 }
 
--- components/templates/TopPage/index.tsx
+++ components/templates/TopPage/index.tsx
@@ -4,6 +4,7 @@ import { NextPage } from 'next'
 import SideBar from '../../organisms/SideBar'
 import { ProfileProps } from '../../molecules/Profile'
 import ArticleCards, { ArticleCardsProps } from '../../organisms/ArticleCards'
+import Footer from '../../organisms/Footer'
 
 type TopPageProps = HeaderProps & ProfileProps & ArticleCardsProps
 
@@ -11,13 +12,16 @@ const TopPage: NextPage<TopPageProps> = (props) => {
 	const { title, src, alt, name, message, cards } = props
 
 	return (
-		<Box display='flex'>
-			<Header title={title} />
-			<SideBar src={src} alt={alt} name={name} message={message} />
-			<Container sx={{ mt: 12 }}>
-				<Typography variant='h4'>New Articles</Typography>
-				<ArticleCards cards={cards} />
-			</Container>
+		<Box display='flex' flexDirection='column'>
+			<Box display='flex'>
+				<Header title={title} />
+				<SideBar src={src} alt={alt} name={name} message={message} />
+				<Container sx={{ mt: 12 }}>
+					<Typography variant='h4'>New Articles</Typography>
+					<ArticleCards cards={cards} />
+				</Container>
+			</Box>
+			<Footer />
 		</Box>
 	)
 }
