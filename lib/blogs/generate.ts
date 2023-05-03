1aa32c62c3b86dc0ec6771eb66a10fd19c4089ce
yutaYAMAMORI <yamamori929yuta@gmail.com>
2023-05-03 12:20:32 +0900

    add: Generate article cards in the top page with the article's meta

---
+++ lib/blogs/generate.ts
@@ -0,0 +1,18 @@
+import path from 'path'
+import { readdirSync, readFileSync } from 'fs'
+import matter from 'gray-matter'
+
+const blogsDir = path.join(process.cwd(), 'pages/blogs')
+
+export const getArticleFileNames = () => {
+	return readdirSync(blogsDir, { withFileTypes: true })
+		.filter((dirent) => dirent.isFile())
+		.map((dirent) => dirent.name)
+}
+
+export const getMetaArticle = (filename: string) => {
+	const fullPath = path.join(blogsDir, filename)
+	const fileContent = readFileSync(fullPath, 'utf8')
+	const { data: meta } = matter(fileContent)
+	return meta
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import TopPage from '@/components/templates/TopPage'
+import { GetStaticProps } from 'next'
+import { getArticleFileNames, getMetaArticle } from '@/lib/blogs/generate'
 
-export default function Home() {
+const Home = (props: any) => {
+	const { blogMeta } = props
 	return (
 		<>
 			<Head>
-				<title>Mountain Forest's blog</title>
+				<title>{"Mountain Forest's blog"}</title>
 				<meta name='description' content="Mountain Forest's blog" />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/mountain-forest-logo.png' />
@@ -16,8 +19,26 @@ export default function Home() {
 				alt='MF'
 				name='Muntain Forest'
 				message="I'm a software engineer."
-				cards={[]}
+				cards={blogMeta}
 			/>
 		</>
 	)
+}
+
+export default Home
+
+export const getStaticProps: GetStaticProps = (context) => {
+	const articleFileNames = getArticleFileNames()
+
+	// TODO: Get article's meta
+	const meta = articleFileNames.map((filename) => {
+		return getMetaArticle(filename)
+	})
+
+	console.log('tmpData', meta)
+	return {
+		props: {
+			blogMeta: meta,
+		},
+	}
 }
