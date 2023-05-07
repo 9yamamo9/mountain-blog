4d73e057ef3f364a12c3fb0f16692ee0cdbf9991
yutaYAMAMORI <yamamori929yuta@gmail.com>
2023-05-07 19:30:40 +0900

    mod: the header title has button for top

--- components/molecules/HeaderTitle/index.tsx
+++ components/molecules/HeaderTitle/index.tsx
@@ -1,12 +1,18 @@
 import { Box, Typography } from '@mui/material'
 import { NextPage } from 'next'
 import { HeaderProps } from '../../organisms/Header'
+import { useRouter } from 'next/router'
 
 const HeaderTitle: NextPage<HeaderProps> = (props) => {
 	const { title } = props
+	const router = useRouter()
+
+	const backTop = async () => {
+		await router.push('/')
+	}
 
 	return (
-		<Box flex={1}>
+		<Box flex={1} onClick={backTop}>
 			<Typography variant='h4'>{title}</Typography>
 		</Box>
 	)
