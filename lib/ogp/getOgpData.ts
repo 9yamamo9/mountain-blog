4eadf27731759b40c4d74d92392fb35a10aa78ee
yutaYAMAMORI <yamamori929yuta@gmail.com>
2023-05-23 22:36:52 +0900

    Add: Create function for getting ogp

---
+++ lib/ogp/getOgpData.ts
@@ -0,0 +1,32 @@
+import {
+	OgObjectInteral,
+	OpenGraphScraperOptions,
+} from 'open-graph-scraper/dist/lib/types'
+import setOptionsAndReturnOpenGraphResults from 'open-graph-scraper/dist/lib/openGraphScraper'
+
+const getOpgData = async (
+	floatingUrls: string[]
+): Promise<OgObjectInteral[]> => {
+	const ogpDataList: OgObjectInteral[] = []
+
+	await Promise.all(
+		floatingUrls.map(async (url) => {
+			const options: OpenGraphScraperOptions = {
+				url: url,
+				onlyGetOpenGraphInfo: true,
+			}
+
+			try {
+				const data = await setOptionsAndReturnOpenGraphResults(options)
+				const result = data.ogObject
+
+				if (!result.success) return
+				ogpDataList.push(result)
+			} catch (e) {
+				console.log(e)
+			}
+		})
+	)
+
+	return ogpDataList
+}
